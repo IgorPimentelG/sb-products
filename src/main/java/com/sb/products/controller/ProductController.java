2dc7ce14d86ea70e05620ff45ddbf3a27e2500ff
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-08-13 03:16:14 -0300

    ✨ feat: add v2 of the route to get all products with pagination

--- src/main/java/com/sb/products/controller/ProductController.java
+++ src/main/java/com/sb/products/controller/ProductController.java
@@ -7,6 +7,9 @@ import com.sb.products.model.dto.product.ProductDTO;
 import com.sb.products.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -58,6 +61,21 @@ public class ProductController {
 	@GetMapping(value = "/v1")
 	public ResponseEntity<List<Product>> findAll() {
 		List<Product> products = service.findAll();
+		products.forEach(item -> {
+			try {
+				item.add(
+				  linkTo(
+					methodOn(ProductController.class).findById(item.getId())).withSelfRel()
+				);
+			} catch (Exception ignored) {}
+		});
+
+		return ResponseEntity.status(HttpStatus.OK).body(products);
+	}
+
+	@GetMapping(value = "/v2")
+	public ResponseEntity<Page<Product>> findAll(@PageableDefault(size = 5) Pageable pageable) {
+		Page<Product> products = service.findAll(pageable);
 		products.forEach(item -> {
 			try {
 				item.add(
--- src/main/java/com/sb/products/services/ProductService.java
+++ src/main/java/com/sb/products/services/ProductService.java
@@ -7,6 +7,8 @@ import com.sb.products.model.dto.product.ProductDTO;
 import com.sb.products.repositories.ProductRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,7 +25,7 @@ public class ProductService {
 
 	public Product create(ProductDTO product) throws ResourceRequiredException {
 		if (product == null) {
-			logger.log(Level.WARNING, "Product cannot be null.");
+			logger.log(Level.WARNING, "[V1] Product cannot be null.");
 			throw new ResourceRequiredException();
 		}
 
@@ -31,7 +33,7 @@ public class ProductService {
 		BeanUtils.copyProperties(product, entity);
 		var createdProduct = repository.save(entity);
 
-		logger.log(Level.INFO, "Product created.");
+		logger.log(Level.INFO, "[V1] Product created.");
 
 		return createdProduct;
 	}
@@ -39,14 +41,14 @@ public class ProductService {
 	public Product update(String id, ProductDTO product) throws ResourceNotFoundException {
 		var entity = repository.findById(id).orElseThrow(
 		  () -> {
-			  logger.log(Level.WARNING, "Product not found.");
+			  logger.log(Level.WARNING, "[V1] Product not found.");
 			  return new ResourceNotFoundException(id);
 		  });
 
 		BeanUtils.copyProperties(product, entity);
 		repository.save(entity);
 
-		logger.log(Level.INFO, "Product updated.");
+		logger.log(Level.INFO, "[V1] Product updated.");
 
 		return entity;
 	}
@@ -54,28 +56,33 @@ public class ProductService {
 	public Product findById(String id) throws ResourceNotFoundException {
 		var entity = repository.findById(id).orElseThrow(
 		  () -> {
-			  logger.log(Level.WARNING, "Product not found.");
+			  logger.log(Level.WARNING, "[V1] Product not found.");
 			  return new ResourceNotFoundException(id);
 		  });
 
-		logger.log(Level.INFO, "Find product.");
+		logger.log(Level.INFO, "[V1] Find product.");
 
 		return entity;
 	}
 
 	public List<Product> findAll() {
-		logger.log(Level.INFO, "Find all products.");
+		logger.log(Level.INFO, "[V1] Find all products.");
 		return repository.findAll();
+	}
+
+	public Page<Product> findAll(Pageable pageable) {
+		logger.log(Level.INFO, "[V2] Find all products.");
+		return repository.findAll(pageable);
 	}
 
 	public void delete(String id) throws ResourceNotFoundException {
 		var entity = repository.findById(id).orElseThrow(
 		  () -> {
-			  logger.log(Level.WARNING, "Product not found.");
+			  logger.log(Level.WARNING, "[V1] Product not found.");
 			  return new ResourceNotFoundException(id);
 		  });
 
 		repository.delete(entity);
-		logger.log(Level.INFO, "Product deleted.");
+		logger.log(Level.INFO, "[V1] Product deleted.");
 	}
 }
