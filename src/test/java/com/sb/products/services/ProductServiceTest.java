c0b4edd4f6355b56741f7979c58da26ee685b30f
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-08-14 00:58:49 -0300

    ✅ test: ensure that be returns empty list when there are no products

--- src/test/java/com/sb/products/services/ProductServiceTest.java
+++ src/test/java/com/sb/products/services/ProductServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -103,6 +104,18 @@ public class ProductServiceTest {
 		assertNotNull(result);
 		assertArrayEquals(products.toArray(), result.toArray());
 		assertEquals(products.size(), result.size());
+	}
+
+	@Test
+	public void testFindAllWithEmptyProducts() throws Exception {
+		List<Product> products = new ArrayList<>();
+
+		when(repository.findAll()).thenReturn(products);
+
+		var result = service.findAll();
+
+		assertNotNull(result);
+		assertEquals(result.size(), 0);
 	}
 
 	@Test
