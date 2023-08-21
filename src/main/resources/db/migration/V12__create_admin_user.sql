abd9849a3bfc7ac6684ebc8f269fc858ac41ec79
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-08-21 16:05:56 -0300

    :hammer: script: add migration to create admin user

---
+++ src/main/resources/db/migration/V12__create_admin_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id, full_name, email, password) VALUES
+    ('eb5fcce4-7871-40d3-ad49-7c43fa6c93d3', 'SB Administrator', 'admin@sb.com', '$2a$12$DRc3XEzAnT31K9oDj2zBWOkp4VgLuC5PxgrMnuLNUHc7RRfEziO06');
+
+INSERT INTO user_permissions (id_user, id_permission) VALUES
+    ('eb5fcce4-7871-40d3-ad49-7c43fa6c93d3', 1)
+
