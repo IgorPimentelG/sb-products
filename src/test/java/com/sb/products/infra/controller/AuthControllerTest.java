858ebf75b4fcfa78b5c0e02b7820459a9c68dffc
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-08-22 11:52:06 -0300

    :test_tube: test: add refresh token tests

--- src/test/java/com/sb/products/infra/controller/AuthControllerTest.java
+++ src/test/java/com/sb/products/infra/controller/AuthControllerTest.java
@@ -13,6 +13,7 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -31,7 +32,8 @@ public class AuthControllerTest extends AbstractIntegration {
 
 	@Test
 	@Order(0)
-	void shouldRegisterCommonUser() {
+	@DisplayName("should register a common user")
+	void testRegisterCommonUser() {
 
 		var body = new UserRegisterDto(
 		  "Any Full Name",
@@ -53,11 +55,10 @@ public class AuthControllerTest extends AbstractIntegration {
 		assertNotNull(result);
 	}
 
-	//
-
 	@Test
 	@Order(1)
-	void shouldNotRegisterCommonUserWithDuplicateEmail() {
+	@DisplayName("shouldn't register a common user with duplicate email")
+	void testRegisterCommonUserWithDuplicateEmail() {
 
 		var body = new UserRegisterDto(
 		  "Any Full Name",
@@ -82,7 +83,8 @@ public class AuthControllerTest extends AbstractIntegration {
 
 	@Test
 	@Order(2)
-	void shouldNotRegisterCommonUserWithInvalidFields() {
+	@DisplayName("shouldn't register a common user with invalid fields")
+	void testRegisterCommonUserWithInvalidFields() {
 
 		var body = new UserRegisterDto(
 		  "",
@@ -107,7 +109,8 @@ public class AuthControllerTest extends AbstractIntegration {
 
 	@Test
 	@Order(3)
-	void shouldRegisterManagerUser() {
+	@DisplayName("should register a manager user")
+	void testRegisterManagerUser() {
 		CredentialsDto credentials = new CredentialsDto("admin@sb.com", "12345678");
 		RequestSpecification specification = loginAdmin();
 
@@ -132,7 +135,8 @@ public class AuthControllerTest extends AbstractIntegration {
 
 	@Test
 	@Order(4)
-	void shouldNotRegisterManagerUserWithDuplicateEmail() {
+	@DisplayName("shouldn't register a manager user with duplicate email")
+	void testRegisterManagerUserWithDuplicateEmail() {
 
 		RequestSpecification specification = loginAdmin();
 
@@ -158,7 +162,8 @@ public class AuthControllerTest extends AbstractIntegration {
 
 	@Test
 	@Order(5)
-	void shouldNotRegisterManagerUserWithInvalidFields() {
+	@DisplayName("shouldn't register a manager user with invalid fields")
+	void testRegisterManagerUserWithInvalidFields() {
 
 		RequestSpecification specification = loginAdmin();
 
@@ -184,7 +189,8 @@ public class AuthControllerTest extends AbstractIntegration {
 
 	@Test
 	@Order(6)
-	void shouldNotRegisterManagerUserWithoutIsAuthenticatedAsAdmin() {
+	@DisplayName("shouldn't register a manager user without is authenticate as admin")
+	void testRegisterManagerUserWithoutIsAuthenticateAsAdmin() {
 
 		var body = new UserRegisterDto(
 		  "Any Full Name",
@@ -201,6 +207,102 @@ public class AuthControllerTest extends AbstractIntegration {
 		  .post()
 		  .then()
 		  .statusCode(403);
+	}
+
+	@Test
+	@Order(7)
+	@DisplayName("should refresh token")
+	void testRefreshToken() {
+		CredentialsDto credentials = new CredentialsDto("admin@sb.com", "12345678");
+
+		var token = given()
+		  .basePath("/api/auth/signin")
+		  .port(TestConstants.SERVER_PORT)
+		  .contentType(TestConstants.CONTENT_TYPE_JSON)
+		  .body(credentials)
+		  .when()
+		  .post()
+		  .then()
+		  .statusCode(200)
+		  .extract().body().as(AuthDto.class).token();
+
+		RequestSpecification specification = new RequestSpecBuilder()
+		  .addHeader("Authorization", "Bearer " + token.accessToken())
+		  .setBasePath("/api/auth/refresh")
+		  .setPort(TestConstants.SERVER_PORT)
+		  .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+		  .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+		  .build();
+
+		var result = given()
+		  .spec(specification)
+		  .contentType(TestConstants.CONTENT_TYPE_JSON)
+		  .header("Origin", TestConstants.ORIGIN_LOCALHOST)
+		  .pathParam("email", "admin@sb.com")
+		  .when()
+		  .put("{email}")
+		  .then()
+		  .statusCode(200)
+		  .extract().body().as(AuthDto.class);
+
+		assertNotNull(result);
+	}
+
+	@Test
+	@Order(8)
+	@DisplayName("shouldn't refresh token of user that does not exists")
+	void testRefreshTokenOfUserThatDoesNotExists() {
+		CredentialsDto credentials = new CredentialsDto("admin@sb.com", "12345678");
+
+		var token = given()
+		  .basePath("/api/auth/signin")
+		  .port(TestConstants.SERVER_PORT)
+		  .contentType(TestConstants.CONTENT_TYPE_JSON)
+		  .body(credentials)
+		  .when()
+		  .post()
+		  .then()
+		  .statusCode(200)
+		  .extract().body().as(AuthDto.class).token();
+
+		RequestSpecification specification = new RequestSpecBuilder()
+		  .addHeader("Authorization", "Bearer " + token.accessToken())
+		  .setBasePath("/api/auth/refresh")
+		  .setPort(TestConstants.SERVER_PORT)
+		  .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+		  .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+		  .build();
+
+		var result = given()
+		  .spec(specification)
+		  .contentType(TestConstants.CONTENT_TYPE_JSON)
+		  .header("Origin", TestConstants.ORIGIN_LOCALHOST)
+		  .pathParam("email", "any@sb.com")
+		  .when()
+		  .put("{email}")
+		  .then()
+		  .statusCode(404)
+		  .extract().body().as(ExceptionResponse.class);
+
+		assertNotNull(result);
+		assertTrue(result.message().contains("Resource with id (any@sb.com) not exists."));
+	}
+
+	@Test
+	@Order(9)
+	@DisplayName("shouldn't refresh token without token")
+	void testRefreshTokenWithoutToken() {
+
+		given()
+		  .basePath("/api/auth/refresh")
+		  .port(TestConstants.SERVER_PORT)
+		  .contentType(TestConstants.CONTENT_TYPE_JSON)
+		  .header("Origin", TestConstants.ORIGIN_LOCALHOST)
+		  .pathParam("email", "admin@sb.com")
+		  .when()
+		  .put("{email}")
+		  .then()
+		  .statusCode(400);
 	}
 
 	private RequestSpecification loginAdmin() {
